059204630dd59c12128fc1626c41673ecce603a3
ivanlolq356 <ivansteksov356@gmail.com>
2024-04-02 00:37:59 +0400

    homeworks
    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,45 @@
-"use strict"
+"use strict";
+
+// TASK 1
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let roots = [];
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
+    return roots;
+  } else if (discriminant === 0) {
+    let root = -b / (2 * a);
+    roots.push(root)
+    return roots;
+  } else {
+    let rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+    roots.push(rootOne);
+    roots.push(rootTwo);
+
+    return roots;
+  }
 }
 
+// TASK 2
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (
+    isNaN(percent) ||
+    isNaN(contribution) ||
+    isNaN(amount) ||
+    isNaN(countMonths) ||
+    percent < 0 ||
+    contribution < 0 ||
+    amount < 0 ||
+    countMonths < 0
+  ) {
+    return false;
+  }
+
+  let monthlyInterestRate = percent / 100 / 12;
+  let loanAmount = amount - contribution;
+  let monthlyPayment = loanAmount * (monthlyInterestRate + monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, countMonths) - 1));
+  let totalAmount = monthlyPayment * countMonths;
+
+  return +(totalAmount.toFixed(2));
 }
