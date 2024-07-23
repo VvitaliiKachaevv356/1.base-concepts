fa223aeb6bf41975f79de63ba4c1c23fe68d335b
Vitaly <VvitaliiKachaevv356@yandex.ru>
2024-07-23 16:24:30 +0400

    Task-Array
    Домашнее задание по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((value, index) => value === arr2[index]);
 }
 
+// console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]));
+// console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4]));
+// console.log(compareArrays([1, 2, 3], [2, 3, 1]));
+// console.log(compareArrays([8, 1, 2], [8, 1, 2]));
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter((user) => user.gender === gender);
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+  const sumOfAges = filteredUsers.reduce((total, user) => total + user.age, 0);
+
+  const averageAge = sumOfAges / filteredUsers.length;
+
+  return averageAge;
 }
